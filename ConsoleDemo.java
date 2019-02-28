7aa1adcdec84adaccb90d783a536b13dc1808b33
Alec leamas <leamas.alec@nowhere.net>
2019-02-28 10:12:33 +0100

    initial release.

---
+++ ConsoleDemo.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import org.omg.CORBA.portable.InputStream;
+
+public class ConsoleDemo {
+
+	public static void main(String[] args) throws IOException {
+		System.out.print("Ange ett tal: ");
+		BufferedReader input = 
+				new BufferedReader(new InputStreamReader(System.in));
+		Scanner scanner = new Scanner(input);
+		if (scanner.hasNextInt()) {
+			int number = scanner.nextInt();
+			System.out.println("Talet är " + number);
+		}
+		else {
+			System.out.println("Det var inget tal?");
+		}
+	}
+}
